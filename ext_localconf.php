0cabb715483548fa7fd662661053e46b81961836
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2023-08-02 16:51:09 +0100

    Update task registration.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,18 @@
 <?php
+use Digicademy\Xmltool\Task\XmlExtractImportTask;
+
 if (!defined ('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
 // Register cache
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_xmltool_recordcache'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_xmltool_recordcache'] = array();
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_xmltool_recordcache'] = [];
 }
 
 // register scheduler task
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Digicademy\\Xmltool\\Task\\XmlExtractImportTask'] = array(
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][XmlExtractImportTask::class] = [
     'extension' => 'xmltool',
     'title' => 'LLL:EXT:' . 'xmltool' . '/Resources/Private/Language/locallang_db.xlf:xmlExtractImportTask.title',
     'description' => 'LLL:EXT:' . 'xmltool' . '/Resources/Private/Language/locallang_db.xlf:xmlExtractImportTask.description',
-);
+];
