78506eb40f624df858f27179da613a660c5c30ef
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2023-08-02 09:29:07 +0100

    Remove deprecated downward compatibility argument.

--- Classes/Controller/XmlimportModuleController.php
+++ Classes/Controller/XmlimportModuleController.php
@@ -414,7 +414,7 @@ class XmlimportModuleController
      * @param ServerRequestInterface $request the current request
      * @return Response the response with the content
      */
-    public function mainAction(ServerRequestInterface $request, ResponseInterface $response = NULL)
+    public function mainAction(ServerRequestInterface $request)
     {
         $GLOBALS['SOBE'] = $this;
         $this->init();
