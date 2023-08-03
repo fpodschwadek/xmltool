a9ba635b734d66171abf7a86ced3faf8461c8d76
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2023-08-03 16:31:18 +0100

    Add extract/import command (to be used with scheduler).

---
+++ Classes/Command/XmlExtractImportCommand.php
@@ -0,0 +1,69 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  Torsten Schrade <Torsten.Schrade@adwmainz.de>, Academy of Sciences and Literature | Mainz
+ *  Frodo Podschwadek <frodo.podschwadek@adwmainz.de>, Academy of Sciences and Literature | Mainz
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+namespace Digicademy\Xmltool\Command;
+
+use Digicademy\Xmltool\Service\XmlExtractImportService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{
+    InputArgument,
+    InputInterface
+};
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class XmlExtractImportCommand extends Command {
+
+    protected $service;
+
+    public function __construct(XmlExtractImportService $service)
+    {
+        $this->service = $service;
+        parent::__construct();
+    }
+
+    /**
+     * Configure the command by defining the name, options and arguments
+     */
+    protected function configure()
+    {
+        $this->setDescription('Extract and import data from HKDB XML dump file.')
+           ->setHelp('This command is primarily meant to be used with the scheduler.');
+    }
+
+    /**
+     * Executes the command for showing sys_log entries
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->service->performExtractJobs();
+        $this->service->performImportJobs();
+        return Command::SUCCESS;
+    }
+}
