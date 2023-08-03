75a9eab9c1168fb4f8c38030f58e005420941862
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2023-08-03 16:30:30 +0100

    Remove old scheduler configuration.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,4 @@
 <?php
-use Digicademy\Xmltool\Task\XmlExtractImportTask;
-
 if (!defined ('TYPO3_MODE')) {
     die ('Access denied.');
 }
@@ -9,10 +7,3 @@ if (!defined ('TYPO3_MODE')) {
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_xmltool_recordcache'])) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_xmltool_recordcache'] = [];
 }
-
-// register scheduler task
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][XmlExtractImportTask::class] = [
-    'extension' => 'xmltool',
-    'title' => 'LLL:EXT:' . 'xmltool' . '/Resources/Private/Language/locallang_db.xlf:xmlExtractImportTask.title',
-    'description' => 'LLL:EXT:' . 'xmltool' . '/Resources/Private/Language/locallang_db.xlf:xmlExtractImportTask.description',
-];
